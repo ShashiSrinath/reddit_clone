a6d6d62981220b3e83c872bd4422c165d8ae9261
Shashi Srinath <atshashisrinth@gmail.com>
2020-11-14 10:27:24 +0530

    fixed a typo on port

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from './core/validation.pipe';
 import { RoleGuard } from './auth/guards/role.guard';
 
 async function bootstrap() {
-  const port = process.env.port ? process.env.port : 4000;
+  const port = process.env.PORT ? process.env.PORT : 4000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalGuards(new RoleGuard(new Reflector()));
