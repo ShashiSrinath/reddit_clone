d076a27ccc49feaec6eb581a7cc6d6a3e7e00c41
Shashi Srinath <atshashisrinth@gmail.com>
2020-11-14 10:21:06 +0530

    added a dynamic port for api

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,13 @@ import { ValidationPipe } from './core/validation.pipe';
 import { RoleGuard } from './auth/guards/role.guard';
 
 async function bootstrap() {
+  const port = process.env.port ? process.env.port : 4000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalGuards(new RoleGuard(new Reflector()));
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(4000);
-  console.log('App started on port: 4000');
+  await app.listen(port);
+  console.log('App started on port:', port);
 }
 bootstrap();
